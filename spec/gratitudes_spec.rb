ee2a52ef2b6e8876a8bcf0c99092356b42191249
Arbnor Sfarca <arbnor-sfarca@hotmail.com>
2023-05-10 12:01:15 +0100

    testing classes challenge completed

--- spec/gratitudes_spec.rb
+++ spec/gratitudes_spec.rb
@@ -4,6 +4,19 @@ RSpec.describe Gratitudes do
   it "initialize gratitudes empty array" do    
     gratitudes = Gratitudes.new
     expect(gratitudes.format).to eq "Be grateful for: "
-  end  
+  end 
+  
+    it "adding a gratitude" do    
+      gratitudes = Gratitudes.new
+      gratitudes.add("life")
+      expect(gratitudes.format).to eq "Be grateful for: life"
+    end
+
+    it "joining strings" do    
+      gratitudes = Gratitudes.new
+      gratitudes.add("life")
+      gratitudes.add("Makers Academy")
+      expect(gratitudes.format).to eq "Be grateful for: life, Makers Academy"
+    end
 end
 
