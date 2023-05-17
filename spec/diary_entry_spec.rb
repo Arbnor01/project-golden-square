c69af5150db3bdbb04f6cf5cff59d32f4f3cf519
Arbnor Sfarca <arbnor-sfarca@hotmail.com>
2023-05-17 13:57:17 +0100

    added contents expect line and @contents on lib file

--- spec/diary_entry_spec.rb
+++ spec/diary_entry_spec.rb
@@ -3,7 +3,8 @@ require "diary_entry"
 RSpec.describe DiaryEntry do 
   it "constructs" do 
     diary_entry = DiaryEntry.new("my_title", "my_contents")
-    expect(diary_entry.title). to eq "my_title"
+    expect(diary_entry.title).to eq "my_title"
+    expect(diary_entry.contents).to eq "my_contents"
   end
 end
 
