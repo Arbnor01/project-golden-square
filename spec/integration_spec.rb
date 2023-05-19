eb857c4913423bc9adfdc7fc281b7dac1eb5f019
Arbnor Sfarca <arbnor-sfarca@hotmail.com>
2023-05-19 18:07:48 +0100

    updated code to add another diary entry

--- spec/integration_spec.rb
+++ spec/integration_spec.rb
@@ -5,9 +5,11 @@ RSpec.describe "integration" do
   context "after adding some entries" do
     it "lists out the entries added" do
       diary = Diary.new 
-      diary1_entry = DiaryEntry.new("my title", "my contents")
-      diary.add(diary1_entry)
-      expect(diary.all).to eq [diary1_entry]
+      diary1_entry_1 = DiaryEntry.new("my title", "my contents")
+      diary1_entry_2 = DiaryEntry.new("my title 2", "my contents 2")
+      diary.add(diary1_entry_1)
+      diary.add(diary1_entry_2)
+      expect(diary.all).to eq [diary1_entry_1, diary1_entry_2]
     end
   end
 end
