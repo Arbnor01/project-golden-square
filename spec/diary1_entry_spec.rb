f28f9ec588093f9096a7fdbc7a61b0db09bc90ce
Arbnor Sfarca <arbnor-sfarca@hotmail.com>
2023-05-19 18:51:38 +0100

    updated code to count the number of word in the contents

--- spec/diary1_entry_spec.rb
+++ spec/diary1_entry_spec.rb
@@ -2,8 +2,15 @@ require 'diary1_entry'
 
 RSpec.describe DiaryEntry do
   it "constructs" do
-    diary_entry = DiaryEntry.new("my_title", "my_contents")
-    expect(diary_entry.title).to eq("my_title")
-    expect(diary_entry.contents).to eq("my_contents")
+      diary_entry = DiaryEntry.new("my_title", "my_contents")
+      expect(diary_entry.title).to eq("my_title")
+      expect(diary_entry.contents).to eq("my_contents")
+    end
+
+  describe "#count_words" do
+    it "counts the number of words in the contents" do
+      diary_entry = DiaryEntry.new("my_title", "one two three four")
+      expect(diary_entry.count_words).to eq(4)
+    end
   end
 end
